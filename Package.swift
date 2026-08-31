5945f5cafdd9fcd1344acd342d2a3a49835ee73d
kyungboshim <kyungbo.shim@neowiz.com>
2026-08-31 13:32:46 +0900

    1.1.10-rc.2

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "REVUPLuckyEvent",
-            url: "https://github.com/revup-tnk/REVUP-iOS-SDK/releases/download/1.1.10-rc.1/REVUPLuckyEvent.zip",
-            checksum: "15f4790528be96bb31c09338cf6d7d1474a7177b9a11b2aa6f7da299ff5041e7"
+            url: "https://github.com/revup-tnk/REVUP-iOS-SDK/releases/download/1.1.10-rc.2/REVUPLuckyEvent.zip",
+            checksum: "0df53f283f9ec2ade3a01979bdaf877524c6103cefe1121e3d61381ad9dd7b5b"
         ),
     ]
 )
